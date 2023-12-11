90a1d89b7ee78fff08ae44f8714466298b84c730
Pavel Zyl <bhnby@yandex.by>
2023-12-11 22:28:04 +0300

    modify Program.cs

--- final11/Program.cs
+++ final11/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+var array = new string[4] { "Жук", "Сук", "Дерево", "Окно" };
+var arrayNew = new string[array.Length];
 
-Console.WriteLine("Hello, World!");
+void FindLength(string[] array, string[] arrayNew)
+{
+    var i = 0;
+    for (var j = 0; j < array.Length; j++)
+        if (array[j].Length <= 3)
+        {
+            arrayNew[i] = array[j];
+            i++;
+        }
+}
+
+FindLength(array, arrayNew);
+
+Console.Write("Start array: ");
+Console.WriteLine(string.Join(" ", array));
+
+Console.Write("Final array: ");
+Console.WriteLine(string.Join(" ", arrayNew));
